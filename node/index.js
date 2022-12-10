0fca33c1c2e0633629ed4660a85fcf839db9cd7a
harikanani <hkanani191@gmail.com>
2022-12-10 21:29:57 +0530

    Auth Module

--- node/index.js
+++ node/index.js
@@ -1,12 +1,33 @@
 require("dotenv").config();
 const express = require("express");
 const http = require("http");
-
 const morgan = require("morgan");
 const parser = require("body-parser");
+const authRouter = require("./Router/authRoutes");
 
 const app = express();
+
 const server = http.createServer(app);
+
+let node_env = process.env.NODE_ENV || "local";
+
+const mongo_url =
+	node_env === "local" ? process.env.MONGO_URL : process.env.MONGO_LIVE;
+
+mongoose.connect(
+	mongo_url,
+	{
+		useNewUrlParser: true,
+		useUnifiedTopology: true,
+	},
+	(d) => {
+		if (d) console.log(`ERROR CONNECTING TO DB ${mongo_url}`, d);
+		console.log(
+			`Connected to ${process.env.NODE_ENV} database: `,
+			`${mongo_url}`,
+		);
+	},
+);
 
 morgan.token("host", function (req) {
 	return req.hostname;
@@ -26,6 +47,8 @@ app.get("/", (req, res) => {
 		message: "Server Up and Running!!!",
 	});
 });
+
+app.use("/auth", authRouter);
 
 app.listen(process.env.PORT, () => {
 	console.log(`server up and running on port ${process.env.PORT}`);
