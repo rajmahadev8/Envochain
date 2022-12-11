319e77aa971325c9037b70ecba3450c3bad042f7
harikanani <hkanani191@gmail.com>
2022-12-11 11:55:57 +0530

    added hedera hashgraph based chat room and twillio authentication

--- node/index.js
+++ node/index.js
@@ -8,6 +8,8 @@ const authRouter = require("./Router/authRoutes");
 const app = express();
 
 const server = http.createServer(app);
+global.socketio = require("socket.io")(server);
+require("./socket")(socketio);
 
 let node_env = process.env.NODE_ENV || "local";
 
